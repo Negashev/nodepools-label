e30f1a92861d27d6f877d57b961b3e2c41df5fbe
Negash <i@negash.ru>
2020-12-28 23:02:01 +0500

    Add preemptible

--- run.py
+++ run.py
@@ -1,16 +1,27 @@
 import asyncio
 import os
 import re
+import argparse
+import datetime
 
 from kubernetes_asyncio import client, config, watch
 from kubernetes_asyncio.config.kube_config import FileOrData
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--preemptible", default=False, action='store_true',
+                    help="set label PREEMPTIBLE_LABEL with taint")
 
 NODEPOOLS = {}
 CLUSTERS = {}
 NODEPOOL_LABEL = os.getenv('NODEPOOL_LABEL', 'cattle.io/nodepool')
 
+FILTER_PREEMPTIBLE_LABEL = os.getenv('FILTER_PREEMPTIBLE_LABEL', 'prepare-preemptible')
+WAIT_TIME_PREEMPTIBLE_LABEL = int(os.getenv('WAIT_TIME_PREEMPTIBLE_LABEL', '23'))
+PREEMPTIBLE_LABEL = os.getenv('PREEMPTIBLE_LABEL', 'preemptible')
 
-async def set_label(obj, hostnamePrefix):
+
+async def set_label(obj, hostnamePrefix, preemptible):
     global NODEPOOL_LABEL
     global CLUSTERS
     if NODEPOOL_LABEL in obj['status']['nodeLabels']:
@@ -27,9 +38,26 @@ async def set_label(obj, hostnamePrefix):
     body = {
         "metadata": {
             "labels": {
-                NODEPOOL_LABEL: hostnamePrefix}
+                NODEPOOL_LABEL: hostnamePrefix
+            }
         }
     }
+    if preemptible:
+        # filter node if prepare-preemptible label not exist
+        if FILTER_PREEMPTIBLE_LABEL in obj['status']['nodeLabels']:
+            return
+        # add PREEMPTIBLE_LABEL if time WAIT_TIME_PREEMPTIBLE_LABEL
+        calculate_time = datetime.datetime.strptime(obj['metadata']['creationTimestamp'],"%Y-%m-%dT%H:%M:%SZ") + datetime.timedelta(hours = WAIT_TIME_PREEMPTIBLE_LABEL)
+        if calculate_time < datetime.datetime.now():
+            body["metadata"]["labels"][PREEMPTIBLE_LABEL] = "true"
+            body["spec"]["internalNodeSpec"]["taints"] = [
+                                                    {
+                                                        "effect": "NoSchedule",
+                                                        "key": datetime.datetime.now().strftime('%s'),
+                                                        "value": "preemptible"
+                                                    }
+                                                ]
+            print(f"set {PREEMPTIBLE_LABEL}=true for node {obj['spec']['requestedHostname']}")
     print(f"set {NODEPOOL_LABEL}={hostnamePrefix} for node {obj['spec']['requestedHostname']}")
     await v1.patch_node(obj['spec']['requestedHostname'], body, _request_timeout=30)
 
@@ -73,7 +101,7 @@ async def simple_watch_clusters():
     CLUSTERS = this_clusters
 
 
-async def simple_watch_nodes():
+async def simple_watch_nodes(preemptible=False):
     global NODEPOOLS
     v1 = client.CustomObjectsApi()
     async with watch.Watch().stream(v1.list_cluster_custom_object, "management.cattle.io", "v3", "nodes",
@@ -83,7 +111,7 @@ async def simple_watch_nodes():
             if obj['spec']['nodePoolName'] and obj['spec']['nodePoolName'] in NODEPOOLS and evt in ["ADDED",
                                                                                                     "MODIFIED"]:
                 try:
-                    await set_label(obj, NODEPOOLS[obj['spec']['nodePoolName']])
+                    await set_label(obj, NODEPOOLS[obj['spec']['nodePoolName']], preemptible)
                 except Exception as e:
                     # can't wait
                     print(f"Wait pool {obj['spec']['nodePoolName']}")
@@ -92,6 +120,7 @@ async def simple_watch_nodes():
 
 
 def main():
+    args = parser.parse_args()
     loop = asyncio.get_event_loop()
 
     # Load the kubeconfig file specified in the KUBECONFIG environment
@@ -100,7 +129,7 @@ def main():
     # loop.run_until_complete(config.load_kube_config())
     loop.run_until_complete(simple_watch_clusters())
     loop.run_until_complete(simple_watch_nodepools())
-    loop.run_until_complete(simple_watch_nodes())
+    loop.run_until_complete(simple_watch_nodes(args.preemptible))
 
     loop.close()
 
